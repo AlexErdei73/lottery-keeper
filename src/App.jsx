7a47ad28975c26cb9c0ec1a9a37e0b6d229b4734
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-09-20 13:45:10 +0100

    Add h1 element to app with little CSS transition

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
   const [state, setState] = useState({});
 
+  /* Little opening animation */
+  useEffect(() => {
+    setTimeout(() => {
+      const titleNode = document.querySelector(".app");
+      titleNode.classList.add("show");
+    }, 0);
+  }, []);
+
   return (
-    <>
+    <div className="app">
       <h1>Lottery Keeper</h1>
-    </>
+    </div>
   );
 }
 
