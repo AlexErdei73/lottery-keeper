6d47aed629a0e72f540966d08772cfe9d02ad2b7
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-09-20 22:38:14 +0100

    Add Entrance button

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,29 @@ import "./App.css";
 import lottoPic from "./assets/images/lotteryticket.avif";
 
 function App() {
-  const [state, setState] = useState({});
+	const [state, setState] = useState({});
 
-  /* Little opening animation */
-  useEffect(() => {
-    setTimeout(() => {
-      const titleNode = document.querySelector(".app");
-      titleNode.classList.add("show");
-    }, 0);
-  }, []);
+	/* Little opening animation */
+	useEffect(() => {
+		setTimeout(() => {
+			const titleNode = document.querySelector(".app");
+			titleNode.classList.add("show");
+		}, 0);
+	}, []);
 
-  return (
-    <div className="app">
-      <h1>Lottery Keeper</h1>
-      <img className="landing-img" src={lottoPic} alt="lottery ticket image" />
-    </div>
-  );
+	return (
+		<div className="app">
+			<h1>Lotto Keeper</h1>
+			<div className="img-container">
+				<img
+					className="landing-img"
+					src={lottoPic}
+					alt="lottery ticket image"
+				/>
+			</div>
+			<button type="button">Entrance</button>
+		</div>
+	);
 }
 
 export default App;
