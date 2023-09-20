b38dfcd2a04de4613f23a6cdc4c0bf72750a3875
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-09-20 23:07:27 +0100

    Add Modal component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import lottoPic from "./assets/images/lotteryticket.avif";
+import Modal from "./components/Modal";
 
 function App() {
 	const [state, setState] = useState({});
+
+	const [openModal, setOpenModal] = useState(false);
+
+	const closeModal = () => setOpenModal(false);
 
 	/* Little opening animation */
 	useEffect(() => {
@@ -23,7 +28,12 @@ function App() {
 					alt="lottery ticket image"
 				/>
 			</div>
-			<button type="button">Entrance</button>
+			<button type="button" onClick={() => setOpenModal(true)}>
+				Entrance
+			</button>
+			<Modal openModal={openModal} closeModal={closeModal}>
+				This is my modal.
+			</Modal>
 		</div>
 	);
 }
