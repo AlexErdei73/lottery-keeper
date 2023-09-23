2d85a4ad56456662c7b1424287c16b9775ac5cdf
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-09-23 22:29:06 +0100

    Save state to localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -6,75 +6,86 @@ import Operator from "./components/Operator";
 import GameImage from "./components/GameImage";
 
 function App() {
-  const [state, setState] = useState({
-    player: {
-      name: "Player",
-      balance: 10000,
-    },
-    operator: {
-      balance: 0,
-    },
-    draws: [],
-    games: [],
-  });
+	const INITIAL_STATE = {
+		player: {
+			name: "Player",
+			balance: 10000,
+		},
+		operator: {
+			balance: 0,
+		},
+		draws: [],
+		games: [],
+	};
+	const [state, setState] = useState(INITIAL_STATE);
 
-  const [page, setPage] = useState("opening");
-  const [openModal, setOpenModal] = useState(false);
+	const [page, setPage] = useState("opening");
+	const [openModal, setOpenModal] = useState(false);
 
-  const closeModal = () => setOpenModal(false);
+	useEffect(() => {
+		const savedState = localStorage.getItem("state");
+		if (!savedState) return;
+		setState(JSON.parse(savedState));
+	}, []);
 
-  const handlePlayerClick = () => setPage("player");
-  const handleOperatorClick = () => setPage("operator");
-  const handleBackClick = () => setPage("opening");
+	useEffect(() => {
+		const stateString = JSON.stringify(state);
+		if (stateString === JSON.stringify(INITIAL_STATE)) return;
+		localStorage.setItem("state", stateString);
+	}, [state]);
 
-  /* Little opening animation */
-  const appNode = useRef();
-  useEffect(() => {
-    setTimeout(() => {
-      appNode.current.classList.add("show");
-    }, 0);
-  }, []);
+	const closeModal = () => setOpenModal(false);
 
-  const openingPage = (
-    <>
-      <h1 className="color-change">Lotto Keeper</h1>
-      <GameImage />
-      <button type="button" onClick={() => setOpenModal(true)}>
-        Entrance
-      </button>
-      <Modal
-        openModal={openModal}
-        closeModal={closeModal}
-        headerText="Question"
-      >
-        Which character are you playing?
-        <div className="buttons">
-          <button type="button" onClick={handlePlayerClick}>
-            Player
-          </button>
-          <button type="button" onClick={handleOperatorClick}>
-            Operator
-          </button>
-        </div>
-      </Modal>
-    </>
-  );
+	const handlePlayerClick = () => setPage("player");
+	const handleOperatorClick = () => setPage("operator");
+	const handleBackClick = () => setPage("opening");
 
-  return (
-    <div className="app" ref={appNode}>
-      {page === "opening" && openingPage}
-      {page === "player" && (
-        <Player
-          state={state}
-          setState={setState}
-          goBack={handleBackClick}
-        ></Player>
-      )}
-      {page === "operator" && (
-        <Operator state={state} goBack={handleBackClick}></Operator>
-      )}
-    </div>
-  );
+	/* Little opening animation */
+	const appNode = useRef();
+	useEffect(() => {
+		setTimeout(() => {
+			appNode.current.classList.add("show");
+		}, 0);
+	}, []);
+
+	const openingPage = (
+		<>
+			<h1 className="color-change">Lotto Keeper</h1>
+			<GameImage />
+			<button type="button" onClick={() => setOpenModal(true)}>
+				Entrance
+			</button>
+			<Modal
+				openModal={openModal}
+				closeModal={closeModal}
+				headerText="Question">
+				Which character are you playing?
+				<div className="buttons">
+					<button type="button" onClick={handlePlayerClick}>
+						Player
+					</button>
+					<button type="button" onClick={handleOperatorClick}>
+						Operator
+					</button>
+				</div>
+			</Modal>
+		</>
+	);
+
+	return (
+		<div className="app" ref={appNode}>
+			{page === "opening" && openingPage}
+			{page === "player" && (
+				<Player
+					state={state}
+					setState={setState}
+					goBack={handleBackClick}></Player>
+			)}
+			{page === "operator" && (
+				<Operator state={state} goBack={handleBackClick}></Operator>
+			)}
+		</div>
+	);
 }
 
 export default App;
