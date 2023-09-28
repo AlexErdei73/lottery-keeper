af9c6ad695d0d6f2a10a488d0da4806b5c6f071b
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-09-28 22:44:01 +0100

    Move back and sort buttons in Ticket to the top

--- src/App.jsx
+++ src/App.jsx
@@ -9,112 +9,108 @@ import Header from "./components/Header";
 import { INITIAL_STATE } from "./helper";
 
 function App() {
-  const [state, setState] = useState(INITIAL_STATE);
+	const [state, setState] = useState(INITIAL_STATE);
 
-  const [page, setPage] = useState("opening");
-  const [openModal, setOpenModal] = useState(false);
-  const [openErrorModal, setOpenErrorModal] = useState(false);
+	const [page, setPage] = useState("opening");
+	const [openModal, setOpenModal] = useState(false);
+	const [openErrorModal, setOpenErrorModal] = useState(false);
 
-  useEffect(() => {
-    const savedState = localStorage.getItem("state");
-    if (!savedState) return;
-    setState(JSON.parse(savedState));
-  }, []);
+	useEffect(() => {
+		const savedState = localStorage.getItem("state");
+		if (!savedState) return;
+		setState(JSON.parse(savedState));
+	}, []);
 
-  useEffect(() => {
-    const stateString = JSON.stringify(state);
-    if (stateString === JSON.stringify(INITIAL_STATE)) return;
-    try {
-      localStorage.setItem("state", stateString);
-    } catch (error) {
-      console.error(error);
-      setOpenErrorModal(true);
-    }
-  }, [state]);
+	useEffect(() => {
+		const stateString = JSON.stringify(state);
+		if (stateString === JSON.stringify(INITIAL_STATE)) return;
+		try {
+			localStorage.setItem("state", stateString);
+		} catch (error) {
+			console.error(error);
+			setOpenErrorModal(true);
+		}
+	}, [state]);
 
-  const closeModal = () => setOpenModal(false);
-  const closeErrorModal = () => setOpenErrorModal(false);
+	const closeModal = () => setOpenModal(false);
+	const closeErrorModal = () => setOpenErrorModal(false);
 
-  const handlePlayerClick = () => setPage("player");
-  const handleOperatorClick = () => setPage("operator");
-  const handleBackClick = () => setPage("opening");
+	const handlePlayerClick = () => setPage("player");
+	const handleOperatorClick = () => setPage("operator");
+	const handleBackClick = () => setPage("opening");
 
-  /* Little opening animation */
-  const mainNode = useRef();
-  useEffect(() => {
-    setTimeout(() => {
-      mainNode.current.classList.add("show");
-    }, 0);
-  }, []);
+	/* Little opening animation */
+	const mainNode = useRef();
+	useEffect(() => {
+		setTimeout(() => {
+			mainNode.current.classList.add("show");
+		}, 0);
+	});
 
-  const openingPage = (
-    <>
-      <Header />
-      <main ref={mainNode}>
-        <p>
-          It is a lottery simulation game. As a player you start with 10000
-          credits and you can buy game tickets. You may make profit if you win.
-          As an operator you start with 0 credits and you trigger the draw. You
-          also can simulate game tickets by the computer and keep the profit.
-          Your goal is to make credit in both case.
-        </p>
-        <GameImage />
-        <button type="button" onClick={() => setOpenModal(true)}>
-          Entrance
-        </button>
-      </main>
-      <Footer />
-      <Modal
-        openModal={openModal}
-        closeModal={closeModal}
-        headerText="Question"
-      >
-        Which character are you playing?
-        <div className="buttons">
-          <button type="button" onClick={handlePlayerClick}>
-            Player
-          </button>
-          <button type="button" onClick={handleOperatorClick}>
-            Operator
-          </button>
-        </div>
-      </Modal>
-    </>
-  );
+	const openingPage = (
+		<>
+			<Header />
+			<main ref={mainNode}>
+				<p>
+					It is a lottery simulation game. As a player you start with 10000
+					credits and you can buy game tickets. You may make profit if you win.
+					As an operator you start with 0 credits and you trigger the draw. You
+					also can simulate game tickets by the computer and keep the profit.
+					Your goal is to make credit in both case.
+				</p>
+				<GameImage />
+				<button type="button" onClick={() => setOpenModal(true)}>
+					Entrance
+				</button>
+			</main>
+			<Footer />
+			<Modal
+				openModal={openModal}
+				closeModal={closeModal}
+				headerText="Question">
+				Which character are you playing?
+				<div className="buttons">
+					<button type="button" onClick={handlePlayerClick}>
+						Player
+					</button>
+					<button type="button" onClick={handleOperatorClick}>
+						Operator
+					</button>
+				</div>
+			</Modal>
+		</>
+	);
 
-  return (
-    <div className="app">
-      {page === "opening" && openingPage}
-      {page === "player" && (
-        <Player
-          state={state}
-          setState={setState}
-          goBack={handleBackClick}
-        ></Player>
-      )}
-      {page === "operator" && (
-        <Operator
-          state={state}
-          setState={setState}
-          goBack={handleBackClick}
-        ></Operator>
-      )}
+	return (
+		<div className="app">
+			{page === "opening" && openingPage}
+			{page === "player" && (
+				<Player
+					state={state}
+					setState={setState}
+					goBack={handleBackClick}></Player>
+			)}
+			{page === "operator" && (
+				<Operator
+					state={state}
+					setState={setState}
+					goBack={handleBackClick}></Operator>
+			)}
 
-      <Modal
-        openModal={openErrorModal}
-        closeModal={closeErrorModal}
-        headerText="Storage Error"
-      >
-        <p>
-          The local storage cannot store more data! Please reset the app to keep
-          playing and able to save the app data in the browser.
-        </p>
-        <button type="button" onClick={closeErrorModal}>
-          OK
-        </button>
-      </Modal>
-    </div>
-  );
+			<Modal
+				openModal={openErrorModal}
+				closeModal={closeErrorModal}
+				headerText="Storage Error">
+				<p>
+					The local storage cannot store more data! Please reset the app to keep
+					playing and able to save the app data in the browser.
+				</p>
+				<button type="button" onClick={closeErrorModal}>
+					OK
+				</button>
+			</Modal>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Tickets.jsx
+++ src/components/Tickets.jsx
@@ -3,115 +3,113 @@ import TicketModal from "./TicketModal";
 import "./tickets.css";
 
 const Tickets = ({ state, goBack, option = "player" }) => {
-  const [index, setIndex] = useState(0);
-  const [openModal, setOpenModal] = useState(false);
-  const [drawIndex, setDrawIndex] = useState(state.draws.length);
+	const [index, setIndex] = useState(0);
+	const [openModal, setOpenModal] = useState(false);
+	const [drawIndex, setDrawIndex] = useState(state.draws.length);
 
-  const getGames = () => {
-    const games = state.games.filter((game) => game.drawIndex === +drawIndex);
-    games.forEach((game, i) => {
-      game.index = i;
-    });
-    return games;
-  };
+	const getGames = () => {
+		const games = state.games.filter((game) => game.drawIndex === +drawIndex);
+		games.forEach((game, i) => {
+			game.index = i;
+		});
+		return games;
+	};
 
-  const [games, setGames] = useState(
-    getGames().filter((game) => game.isPlayer)
-  );
-  const [simuGames, setSimuGames] = useState(
-    getGames().filter((game) => !game.isPlayer)
-  );
+	const [games, setGames] = useState(
+		getGames().filter((game) => game.isPlayer)
+	);
+	const [simuGames, setSimuGames] = useState(
+		getGames().filter((game) => !game.isPlayer)
+	);
 
-  function getAllRewards() {
-    return games.reduce((acc, game) => acc + game.creditReward, 0);
-  }
+	function getAllRewards() {
+		return games.reduce((acc, game) => acc + game.creditReward, 0);
+	}
 
-  const handleTicketClick = (event) => {
-    const index = +event.target.getAttribute("data-index");
-    setIndex(index);
-    setOpenModal(true);
-  };
+	const handleTicketClick = (event) => {
+		const index = +event.target.getAttribute("data-index");
+		setIndex(index);
+		setOpenModal(true);
+	};
 
-  const handleSortClick = () => {
-    const newGames = [...games];
-    const sortedGames = newGames.sort(
-      (gameA, gameB) => gameB.numberOfHits - gameA.numberOfHits
-    );
-    setGames(sortedGames);
-  };
+	const handleSortClick = () => {
+		const newGames = [...games];
+		const sortedGames = newGames.sort(
+			(gameA, gameB) => gameB.numberOfHits - gameA.numberOfHits
+		);
+		setGames(sortedGames);
+	};
 
-  useEffect(() => {
-    setGames(getGames().filter((game) => game.isPlayer));
-    setSimuGames(getGames().filter((game) => !game.isPlayer));
-  }, [state, drawIndex]);
+	useEffect(() => {
+		setGames(getGames().filter((game) => game.isPlayer));
+		setSimuGames(getGames().filter((game) => !game.isPlayer));
+	}, [state, drawIndex]);
 
-  return (
-    <>
-      <h1 className="color-change">Tickets</h1>
-      <div>
-        All Winnings: <output>{getAllRewards()}</output>
-      </div>
-      <div className="form-input">
-        <label htmlFor="drawIndex">Draw Number:*</label>
-        <input
-          type="number"
-          id="drawIndex"
-          min="1"
-          max={state.draws.length + 1}
-          value={drawIndex + 1}
-          onChange={(event) => setDrawIndex(event.target.value - 1)}
-          required
-        />
-      </div>
-      {option !== "player" && <div>{state.player.name}'s Games:</div>}
-      {!!games.length && (
-        <div className="buttons tickets">
-          {games.map((game) => (
-            <button
-              type="button"
-              key={game.index}
-              data-index={game.index}
-              onClick={handleTicketClick}
-            >
-              {game.index + 1}
-            </button>
-          ))}
-        </div>
-      )}
-      {option !== "player" && <div>Simulation Games:</div>}
-      {!!simuGames.length && option !== "player" && (
-        <div className="buttons tickets">
-          {simuGames.map((game, index) => (
-            <button
-              type="button"
-              key={game.index}
-              data-index={game.index}
-              onClick={handleTicketClick}
-            >
-              {game.index + 1}
-            </button>
-          ))}
-        </div>
-      )}
-      <div className="buttons">
-        {option === "player" && (
-          <button type="button" onClick={handleSortClick}>
-            Sort
-          </button>
-        )}
-        <button type="button" onClick={goBack}>
-          Back
-        </button>
-      </div>
+	return (
+		<>
+			<h1 className="color-change">Tickets</h1>
+			<div className="buttons">
+				{option === "player" && (
+					<button type="button" onClick={handleSortClick}>
+						Sort
+					</button>
+				)}
+				<button type="button" onClick={goBack}>
+					Back
+				</button>
+			</div>
+			<div>
+				All Winnings: <output>{getAllRewards()}</output>
+			</div>
+			<div className="form-input">
+				<label htmlFor="drawIndex">Draw Number:*</label>
+				<input
+					type="number"
+					id="drawIndex"
+					min="1"
+					max={state.draws.length + 1}
+					value={drawIndex + 1}
+					onChange={(event) => setDrawIndex(event.target.value - 1)}
+					required
+				/>
+			</div>
+			{option !== "player" && <div>{state.player.name}'s Games:</div>}
+			{!!games.length && (
+				<div className="buttons tickets">
+					{games.map((game) => (
+						<button
+							type="button"
+							key={game.index}
+							data-index={game.index}
+							onClick={handleTicketClick}>
+							{game.index + 1}
+						</button>
+					))}
+				</div>
+			)}
+			{option !== "player" && <div>Simulation Games:</div>}
+			{!!simuGames.length && option !== "player" && (
+				<div className="buttons tickets">
+					{simuGames.map((game, index) => (
+						<button
+							type="button"
+							key={game.index}
+							data-index={game.index}
+							onClick={handleTicketClick}>
+							{game.index + 1}
+						</button>
+					))}
+				</div>
+			)}
 
-      <TicketModal
-        openModal={openModal}
-        closeModal={() => setOpenModal(false)}
-        game={getGames()[index]}
-        index={index}
-      />
-    </>
-  );
+			<TicketModal
+				openModal={openModal}
+				closeModal={() => setOpenModal(false)}
+				game={getGames()[index]}
+				index={index}
+			/>
+		</>
+	);
 };
 
 export default Tickets;
